a36a4ebd8abf7f74f1d0a5d311c5e7cd761a9712
shaminito <shaminito@gmail.com>
2018-11-23 12:37:48 +0100

    Proyecto finalizado.

--- AppLoginUser/src/view/CMainApp.java
+++ AppLoginUser/src/view/CMainApp.java
@@ -1,27 +1,14 @@
 package view;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 
-public class CMainApp implements Initializable{
+public class CMainApp{
 
 	@FXML
 	Button button;
 	
-	@FXML
-	AnchorPane ocapacity;
-	
 	public MainApp mainapp;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-			ocapacity.setVisible(false);
-	}
 	
 	@FXML
 	private void c_btnLogin() {
